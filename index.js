334dbdef2a81bc2c3065f0961d6acf52b2417167
Valeri Sabev <valchygaming@gmail.com>
2023-06-14 04:23:46 +0200

    API Task

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+function middleware(req, res, next) {
+	// Check authorization header
+	const authHeader = req.headers.authorization;
+
+	if (authHeader !== 'jesting') res.status(401).json({ error: 'Unauthorized' });
+	else next();
+}
+
+// Endpoint to get favourite drink
+app.get('/api/favourite-drink', middleware, (req, res) => {
+	const favouriteDrink = 'espresso';
+	res.json({ data: { favouriteCoffee: favouriteDrink } });
+});
+
+// Endpoint to post favourite drink
+app.post('/api/favourite-drink', middleware, (req, res) => {
+	const top3 = req.body.data.top3;
+	console.log('Received top 3 drinks:', top3);
+	res.json({ message: 'Favourite drink posted successfully' });
+});
+
+// Endpoint to get leaderboard of favourite drinks
+app.get('/api/favourite-drinks-leaderboard', middleware, (req, res) => {
+	const top3 = ['espresso', 'cappuccino', 'latte'];
+	res.json({ data: { top3 } });
+});
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Server running on port http://localhost:${port}`);
+});
